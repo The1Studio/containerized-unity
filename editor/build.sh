05a904b782ed2254a981ad8a689faf2049a63c9b
H3nr1-D14z <leduchieu101@gmail.com>
2025-05-14 17:36:36 +0700

    update build script
    android

--- editor/build.sh
+++ editor/build.sh
@@ -10,10 +10,26 @@ UNITY_SCRIPTING_DEFINE_SYMBOLS=${UNITY_SCRIPTING_DEFINE_SYMBOLS:-}
 UNITY_ADDRESSABLES_BUILD_PATH=${UNITY_ADDRESSABLES_BUILD_PATH:-/addressables}
 UNITY_ADDRESSABLES_LOAD_PATH=${UNITY_ADDRESSABLES_LOAD_PATH:-/addressables}
 UNITY_LOG_FILE=${UNITY_LOG_FILE:-/dev/stdout}
+# Parameters for Android builds
+DEVELOPMENT=${DEVELOPMENT:-false}
+OPTIMIZE_SIZE=${OPTIMIZE_SIZE:-false}
+BUILD_APP_BUNDLE=${BUILD_APP_BUNDLE:-false}
+KEYSTORE_NAME=${KEYSTORE_NAME:-}
+KEYSTORE_PASS=${KEYSTORE_PASS:-}
+KEYSTORE_ALIAS_NAME=${KEYSTORE_ALIAS_NAME:-}
+KEYSTORE_ALIAS_PASS=${KEYSTORE_ALIAS_PASS:-}
+
+# Parameters for iOS builds
+
+
+# Parameters for WebGL builds
+
+
 source /active_lic.sh
 
 # Build the project
-unity-editor \
+UNITY_CMD=(
+    unity-editor \
     -batchmode -nographics\
     -quit \
     -executeMethod "Build.BuildFromCommandLine" \
@@ -26,6 +42,23 @@ unity-editor \
     -remoteAddressableBuildPath "$UNITY_ADDRESSABLES_BUILD_PATH" \
     -remoteAddressableLoadPath "$UNITY_ADDRESSABLES_LOAD_PATH" \
     -scriptingDefineSymbols "${UNITY_SCRIPTING_DEFINE_SYMBOLS}"
+)
+
+if [ "$DEVELOPMENT" = "true" ]; then
+    UNITY_CMD+=( -development )
+fi
+if [ "$OPTIMIZE_SIZE" = "true" ]; then
+    UNITY_CMD+=( -optimizeSize )
+fi
+if [ "$BUILD_APP_BUNDLE" = "true" ]; then
+    UNITY_CMD+=( -buildAppBundle )
+    [ -n "$KEYSTORE_NAME" ] && UNITY_CMD+=( -keyStoreFileName "$KEYSTORE_NAME" )
+    [ -n "$KEYSTORE_PASS" ] && UNITY_CMD+=( -keyStorePassword "$KEYSTORE_PASS" )
+    [ -n "$KEYSTORE_ALIAS_NAME" ] && UNITY_CMD+=( -keyStoreAliasName "$KEYSTORE_ALIAS_NAME" )
+    [ -n "$KEYSTORE_ALIAS_PASS" ] && UNITY_CMD+=( -keyStoreAliasPassword "$KEYSTORE_ALIAS_PASS" )
+fi
+
+"${UNITY_CMD[@]}"
 
 # Get the exit code of the build
 BUILD_EXIT_CODE=$?
