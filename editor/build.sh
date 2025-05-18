f5eb621cba3dd16a823416c9d6bd0e9719a8f24b
H3nr1-D14z <leduchieu101@gmail.com>
2025-05-19 03:06:42 +0700

    update

--- editor/build.sh
+++ editor/build.sh
@@ -1,1 +1,95 @@
-#!/usr/bin/env bash# Set the default values for the environment variablesUNITY_PROJECT_PATH=${UNITY_PROJECT_PATH:-/project}UNITY_OUTPUT_PATH=${UNITY_OUTPUT_PATH:-/output}UNITY_BUILD_TARGET=${UNITY_BUILD_TARGET:-Android}UNITY_PLATFORMS=${UNITY_PLATFORMS:-android}UNITY_SCRIPTING_BACKEND=${UNITY_SCRIPTING_BACKEND:-il2cpp}UNITY_SCRIPTING_DEFINE_SYMBOLS=${UNITY_SCRIPTING_DEFINE_SYMBOLS:-}UNITY_ADDRESSABLES_BUILD_PATH=${UNITY_ADDRESSABLES_BUILD_PATH:-/addressables}UNITY_ADDRESSABLES_LOAD_PATH=${UNITY_ADDRESSABLES_LOAD_PATH:-/addressables}UNITY_LOG_FILE=${UNITY_LOG_FILE:-/dev/stdout}# Parameters for Android buildsDEVELOPMENT=${DEVELOPMENT:-false}OPTIMIZE_SIZE=${OPTIMIZE_SIZE:-false}BUILD_APP_BUNDLE=${BUILD_APP_BUNDLE:-false}KEYSTORE_NAME=${KEYSTORE_NAME:-}KEYSTORE_PASS=${KEYSTORE_PASS:-}KEYSTORE_ALIAS_NAME=${KEYSTORE_ALIAS_NAME:-}KEYSTORE_ALIAS_PASS=${KEYSTORE_ALIAS_PASS:-}# Parameters for iOS buildsIOS_SIGNING_TEAM_ID=${IOS_SIGNING_TEAM_ID:-}IOS_TARGET_OS_VERSION=${IOS_TARGET_OS_VERSION:-}# Parameters for WebGL buildsWEBGL_ORIENTATION=${WEBGL_ORIENTATION:-}FACEBOOK_APP_ID=${FACEBOOK_APP_ID:-}FACEBOOK_APP_SECRET=${FACEBOOK_APP_SECRET:-}UPLOAD_TO_FACEBOOK=${UPLOAD_TO_FACEBOOK:-false}source /active_lic.sh# Build the projectUNITY_CMD=(    unity-editor \    -batchmode -nographics\    -quit \    -executeMethod "Build.BuildFromCommandLine" \    -buildTarget "${UNITY_BUILD_TARGET}" \    -platforms "${UNITY_PLATFORMS}" \    -scriptingBackend "${UNITY_SCRIPTING_BACKEND}" \    -projectPath "$UNITY_PROJECT_PATH" \    -logFile "$UNITY_LOG_FILE" \    -outputPath "$UNITY_OUTPUT_PATH" \    -remoteAddressableBuildPath "$UNITY_ADDRESSABLES_BUILD_PATH" \    -remoteAddressableLoadPath "$UNITY_ADDRESSABLES_LOAD_PATH" \    -scriptingDefineSymbols "${UNITY_SCRIPTING_DEFINE_SYMBOLS}")# Common parameters for all platformsif [ "$DEVELOPMENT" = "true" ]; then    UNITY_CMD+=( -development )fiif [ "$OPTIMIZE_SIZE" = "true" ]; then    UNITY_CMD+=( -optimizeSize )fi# Platform-specific parameterscase "$UNITY_BUILD_TARGET" in    "Android")        if [ "$BUILD_APP_BUNDLE" = "true" ]; then            UNITY_CMD+=( -buildAppBundle )            [ -n "$KEYSTORE_NAME" ] && UNITY_CMD+=( -keyStoreFileName "$KEYSTORE_NAME" )            [ -n "$KEYSTORE_PASS" ] && UNITY_CMD+=( -keyStorePassword "$KEYSTORE_PASS" )            [ -n "$KEYSTORE_ALIAS_NAME" ] && UNITY_CMD+=( -keyStoreAliasName "$KEYSTORE_ALIAS_NAME" )            [ -n "$KEYSTORE_ALIAS_PASS" ] && UNITY_CMD+=( -keyStoreAliasPassword "$KEYSTORE_ALIAS_PASS" )        fi        ;;    "iOS")        [ -n "$IOS_SIGNING_TEAM_ID" ] && UNITY_CMD+=( -iosSigningTeamId "$IOS_SIGNING_TEAM_ID" )        [ -n "$IOS_TARGET_OS_VERSION" ] && UNITY_CMD+=( -iosTargetOSVersion "$IOS_TARGET_OS_VERSION" )        ;;    "WebGL")        [ -n "$WEBGL_ORIENTATION" ] && UNITY_CMD+=( -webglOrientation "$WEBGL_ORIENTATION" )        [ -n "$FACEBOOK_APP_ID" ] && UNITY_CMD+=( -facebookAppId "$FACEBOOK_APP_ID" )        [ -n "$FACEBOOK_APP_SECRET" ] && UNITY_CMD+=( -facebookAppSecret "$FACEBOOK_APP_SECRET" )        [ "$UPLOAD_TO_FACEBOOK" = "true" ] && UNITY_CMD+=( -uploadToFacebook )        ;;esac"${UNITY_CMD[@]}"# Get the exit code of the buildBUILD_EXIT_CODE=$?if [ $BUILD_EXIT_CODE -eq 0 ]; then  echo "Build succeeded";else  echo "Build failed, with exit code $BUILD_EXIT_CODE";fisource /return_lic.sh
+#!/usr/bin/env bash
+# Set the default values for the environment variables
+UNITY_PROJECT_PATH=${UNITY_PROJECT_PATH:-/project}
+UNITY_OUTPUT_PATH=${UNITY_OUTPUT_PATH:-/output}
+UNITY_BUILD_TARGET=${UNITY_BUILD_TARGET:-Android}
+UNITY_PLATFORMS=${UNITY_PLATFORMS:-android}
+UNITY_SCRIPTING_BACKEND=${UNITY_SCRIPTING_BACKEND:-il2cpp}
+UNITY_SCRIPTING_DEFINE_SYMBOLS=${UNITY_SCRIPTING_DEFINE_SYMBOLS:-}
+UNITY_ADDRESSABLES_BUILD_PATH=${UNITY_ADDRESSABLES_BUILD_PATH:-/addressables}
+UNITY_ADDRESSABLES_LOAD_PATH=${UNITY_ADDRESSABLES_LOAD_PATH:-/addressables}
+UNITY_LOG_FILE=${UNITY_LOG_FILE:-/dev/stdout}
+
+# Parameters for Android builds
+DEVELOPMENT=${DEVELOPMENT:-false}
+OPTIMIZE_SIZE=${OPTIMIZE_SIZE:-false}
+BUILD_APP_BUNDLE=${BUILD_APP_BUNDLE:-false}
+KEYSTORE_NAME=${KEYSTORE_NAME:-}
+KEYSTORE_PASS=${KEYSTORE_PASS:-}
+KEYSTORE_ALIAS_NAME=${KEYSTORE_ALIAS_NAME:-}
+KEYSTORE_ALIAS_PASS=${KEYSTORE_ALIAS_PASS:-}
+
+# Parameters for iOS builds
+IOS_SIGNING_TEAM_ID=${IOS_SIGNING_TEAM_ID:-}
+IOS_TARGET_OS_VERSION=${IOS_TARGET_OS_VERSION:-}
+
+# Parameters for WebGL builds
+WEBGL_ORIENTATION=${WEBGL_ORIENTATION:-}
+FACEBOOK_APP_ID=${FACEBOOK_APP_ID:-}
+FACEBOOK_APP_SECRET=${FACEBOOK_APP_SECRET:-}
+UPLOAD_TO_FACEBOOK=${UPLOAD_TO_FACEBOOK:-false}
+
+source /active_lic.sh
+
+# Build the project
+UNITY_CMD=(
+    unity-editor \
+    -batchmode -nographics\
+    -quit \
+    -executeMethod "Build.BuildFromCommandLine" \
+    -buildTarget "${UNITY_BUILD_TARGET}" \
+    -platforms "${UNITY_PLATFORMS}" \
+    -scriptingBackend "${UNITY_SCRIPTING_BACKEND}" \
+    -projectPath "$UNITY_PROJECT_PATH" \
+    -logFile "$UNITY_LOG_FILE" \
+    -outputPath "$UNITY_OUTPUT_PATH" \
+    -remoteAddressableBuildPath "$UNITY_ADDRESSABLES_BUILD_PATH" \
+    -remoteAddressableLoadPath "$UNITY_ADDRESSABLES_LOAD_PATH" \
+    -scriptingDefineSymbols "${UNITY_SCRIPTING_DEFINE_SYMBOLS}"
+)
+
+# Common parameters for all platforms
+if [ "$DEVELOPMENT" = "true" ]; then
+    UNITY_CMD+=( -development )
+fi
+
+if [ "$OPTIMIZE_SIZE" = "true" ]; then
+    UNITY_CMD+=( -optimizeSize )
+fi
+
+# Platform-specific parameters
+case "$UNITY_BUILD_TARGET" in
+    "Android")
+        if [ "$BUILD_APP_BUNDLE" = "true" ]; then
+            UNITY_CMD+=( -buildAppBundle )
+            [ -n "$KEYSTORE_NAME" ] && UNITY_CMD+=( -keyStoreFileName "$KEYSTORE_NAME" )
+            [ -n "$KEYSTORE_PASS" ] && UNITY_CMD+=( -keyStorePassword "$KEYSTORE_PASS" )
+            [ -n "$KEYSTORE_ALIAS_NAME" ] && UNITY_CMD+=( -keyStoreAliasName "$KEYSTORE_ALIAS_NAME" )
+            [ -n "$KEYSTORE_ALIAS_PASS" ] && UNITY_CMD+=( -keyStoreAliasPassword "$KEYSTORE_ALIAS_PASS" )
+        fi
+        ;;
+    "iOS")
+        [ -n "$IOS_SIGNING_TEAM_ID" ] && UNITY_CMD+=( -iosSigningTeamId "$IOS_SIGNING_TEAM_ID" )
+        [ -n "$IOS_TARGET_OS_VERSION" ] && UNITY_CMD+=( -iosTargetOSVersion "$IOS_TARGET_OS_VERSION" )
+        ;;
+    "WebGL")
+        [ -n "$WEBGL_ORIENTATION" ] && UNITY_CMD+=( -webglOrientation "$WEBGL_ORIENTATION" )
+        [ -n "$FACEBOOK_APP_ID" ] && UNITY_CMD+=( -facebookAppId "$FACEBOOK_APP_ID" )
+        [ -n "$FACEBOOK_APP_SECRET" ] && UNITY_CMD+=( -facebookAppSecret "$FACEBOOK_APP_SECRET" )
+        [ "$UPLOAD_TO_FACEBOOK" = "true" ] && UNITY_CMD+=( -uploadToFacebook )
+        ;;
+esac
+
+"${UNITY_CMD[@]}"
+
+# Get the exit code of the build
+BUILD_EXIT_CODE=$?
+if [ $BUILD_EXIT_CODE -eq 0 ]; then
+  echo "Build succeeded";
+else
+  echo "Build failed, with exit code $BUILD_EXIT_CODE";
+fi
+
+source /return_lic.sh
+
+
--- editor/return_lic.sh
+++ editor/return_lic.sh
@@ -1,1 +1,25 @@
-#!/usr/bin/env bashUNITY_EMAIL=${UNITY_EMAIL:-}UNITY_PASSWORD=${UNITY_PASSWORD:-}# Return licenseunity-editor \    -logFile /tmp/unity-return.log \    -quit \    -returnlicense \    -username "$UNITY_EMAIL" \    -password "$UNITY_PASSWORD" \    -projectPath /home/jenkins/BlankProject# Store the exit code from the verify commandUNITY_EXIT_CODE=$?# Check if UNITY_EXIT_CODE is 0if [[ $UNITY_EXIT_CODE -eq 0 ]]then  echo "Return successful"else  echo "Return failed"fi
+#!/usr/bin/env bash
+UNITY_EMAIL=${UNITY_EMAIL:-}
+UNITY_PASSWORD=${UNITY_PASSWORD:-}
+
+# Return license
+unity-editor \
+    -logFile /tmp/unity-return.log \
+    -quit \
+    -returnlicense \
+    -username "$UNITY_EMAIL" \
+    -password "$UNITY_PASSWORD" \
+    -projectPath /home/jenkins/BlankProject
+
+# Store the exit code from the verify command
+UNITY_EXIT_CODE=$?
+
+# Check if UNITY_EXIT_CODE is 0
+if [[ $UNITY_EXIT_CODE -eq 0 ]]
+then
+  echo "Return successful"
+else
+  echo "Return failed"
+fi
+
+
